d9e4a771535a316519327b651ec3dc8934ef617f
efoejean <77749280+efoejean@users.noreply.github.com>
2021-11-17 22:11:54 -0600

    Create a review using the controller

--- server/app/controller.js
+++ server/app/controller.js
@@ -1,6 +1,7 @@
 // Uses the db client from loader.js
 import client from "./client.js";
 import config from "./config.js";
+import { v4 as uuidv4 } from "uuid";
 
 
 const collection = client.db(config.db.name).collection(config.db.collection);
@@ -10,5 +11,13 @@ export default{
        
       return collection.find({}).limit(Number(queryParams.limit) || 50).toArray();
     },
+
+    createReview(listingId, newReview) { 
+        newReview._id = uuidv4(); // Generate a unique id for the review
+        return collection.updateOne(
+          { _id: listingId },
+          { $push: { reviews: newReview } } // Add the review to the reviews array
+        );
+      },
 };
 
